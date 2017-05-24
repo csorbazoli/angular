034efb7cab7ff73e3016888d3bc0f13122515f32
csorbazoli <csorbazoli@freemail.hu>
2017-05-24 21:06:58 +0100

    css, minor changes

--- gepiras/WebContent/js/typewriter.js
+++ gepiras/WebContent/js/typewriter.js
@@ -1,22 +1,23 @@
-angular.module('mainWindow', []).controller('MainController', 
-function MainController($scope) {
-	// get last user from cookie and load data from DB
-	this.active_profile = {
-			name : 'Zoli',
-			registrationDate: '2017-04-10 21:19:36',
-			lastLesson: 0,
-			lastExercise: 0
-	};
-	this.new_profile = {
-			name : 'Új...',
-			registrationDate: null,
-			lastLesson: 0,
-			lastExercise: 0
-	};
-	this.profiles = [this.active_profile, this.new_profile];
-	$scope.profil = this.active_profile.name;
-	
-	this.selectProfile = function() {
-		console.log("Profile selected: "+$scope.profil);
-	};
-});
+angular.module('mainWindow', [])
+.controller('MainController', 
+	function MainController($scope) {
+		// get last user from cookie and load data from DB
+		this.active_profile = {
+				name : 'Zoli',
+				registrationDate: '2017-04-10 21:19:36',
+				lastLesson: 0,
+				lastExercise: 0
+		};
+		this.new_profile = {
+				name : 'Új...',
+				registrationDate: null,
+				lastLesson: 0,
+				lastExercise: 0
+		};
+		this.profiles = [this.active_profile, this.new_profile];
+		$scope.profil = this.active_profile.name;
+		
+		this.selectProfile = function() {
+			console.log("Profile selected: "+$scope.profil);
+		};
+	});
