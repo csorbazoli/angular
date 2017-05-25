aad90ca55784f3e9120e2214768e849d6a1858bb
csorbazoli <csorbazoli@freemail.hu>
2017-05-25 06:57:25 +0100

    use scope instead of main (this)

--- gepiras/WebContent/js/typewriter.js
+++ gepiras/WebContent/js/typewriter.js
@@ -2,22 +2,22 @@ angular.module('mainWindow', [])
 .controller('MainController', 
 	function MainController($scope) {
 		// get last user from cookie and load data from DB
-		this.active_profile = {
+		$scope.active_profile = {
 				name : 'Zoli',
 				registrationDate: '2017-04-10 21:19:36',
 				lastLesson: 0,
 				lastExercise: 0
 		};
-		this.new_profile = {
+		$scope.new_profile = {
 				name : 'Új...',
 				registrationDate: null,
 				lastLesson: 0,
 				lastExercise: 0
 		};
-		this.profiles = [this.active_profile, this.new_profile];
-		$scope.profil = this.active_profile.name;
+		$scope.profiles = [$scope.active_profile, $scope.new_profile];
+		$scope.profil = $scope.active_profile.name;
 		
-		this.selectProfile = function() {
+		$scope.selectProfile = function() {
 			console.log("Profile selected: "+$scope.profil);
 		};
 	});
